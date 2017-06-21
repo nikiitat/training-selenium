3925550d46862848c7d1a4409bd4d05e3f955c4d
Nikita Tertytskyi <nikita.tertytskyi@gmail.com>
2017-06-21 16:15:36 +0200

    final version of adminTest

--- src/test/java/stqa/MyAdminTest.java
+++ src/test/java/stqa/MyAdminTest.java
@@ -5,9 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -18,8 +15,17 @@ import static org.junit.Assert.assertTrue;
  */
 public class MyAdminTest {
     private WebDriver driver;
-    private WebDriverWait wait;
-    private WebElement element;
+
+    boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (InvalidSelectorException ex){
+            throw ex;
+        } catch (TimeoutException ex) {
+            return false;
+        }
+    }
 
     @Before
     public void start(){
@@ -27,7 +33,6 @@ public class MyAdminTest {
 //                "C:\\Tools\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 10);
         driver.get("http://localhost/litecart/admin/login.php");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
@@ -36,31 +41,29 @@ public class MyAdminTest {
     }
 
     @Test
-    public void myAdminFirstTest(){
-        List<WebElement> links = driver.findElements(By.xpath("//li[@id='app-']//*[@class='name']"));
+    public void myAdminFirstTest() {
+        verifyTitlePresentForAllMenuLinks();
+    }
+
+     public void verifyTitlePresentForAllMenuLinks(){
+        List<WebElement> links = driver.findElements(By.xpath("//li[@id='app-']/a/span[@class='name']"));
         List<WebElement> links2;
-        System.out.println(links.size());
 
         for(int i=0;i<links.size();i++)
         {
             if(!(links.get(i).getText().isEmpty()))
             {
-                System.out.println(links.get(i).getText());
                 links.get(i).click();
-//                WebElement element =driver.findElement(By.xpath("//li[@id='app-']//*[@class='name']"));
+                assertTrue(isElementPresent(By.xpath("//h1")));
                 links2 = driver.findElements(By.xpath("//li[@id='app-']//*[@id]"));
-                System.out.println(links2.get(1).getText());
-                if (links2.size() > 0){
                 for (int a = 0; a < links2.size(); a++) {
                     if (!(links2.get(a).getText().isEmpty())) {
-                        System.out.println(links2.get(a).getText());
                         links2.get(a).click();
+                        assertTrue(isElementPresent(By.xpath("//h1")));
+                        links2 = driver.findElements(By.xpath("//li[@id='app-']//*[@id]"));
                     }
                 }
-            }
-                driver.navigate().back();
-                links=driver.findElements(By.xpath("//li[@id='app-']//*[@class='name']"));
-
+                links=driver.findElements(By.xpath("//li[@id='app-']/a/span[@class='name']"));
             }
         }
     }
--- src/test/java/stqa/TestSticker.java
+++ src/test/java/stqa/TestSticker.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.*;
+
+import java.util.List;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -41,56 +44,56 @@ public class TestSticker {
 
     @Test
     public void myAdminFirstTest(){
-//        List<WebElement> rows = driver.findElements(By.xpath("//*[@id='box-most-popular']//*[@title='Yellow Duck']//*[@class='sticker sale']"));
-//        System.out.println("Total selected rows are " + rows.size());
+        List<WebElement> rows = driver.findElements(By.xpath("//*[@id='box-most-popular']//*[@title='Yellow Duck']//*[@class='sticker sale']"));
+        System.out.println("Total selected rows are " + rows.size());
         //Yellow Duck
-        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-most-popular']//*[@title='Yellow Duck']//*[@class='sticker sale']")));
-        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Yellow " +
-                "Duck']//*[@class='sticker new']")));
-        //Green Duck
-        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-most-popular']//*[@title='Green Duck']//*[@class='sticker new']")));
-        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Green " +
-                "Duck']//*[@class='sticker sale']")));
-        //Purple Duck
-        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-most-popular']//*[@title='Purple Duck']//*[@class='sticker new']")));
-        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Purple " +
-                "Duck']//*[@class='sticker sale']")));
-        //Red Duck
-        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-most-popular']//*[@title='Red Duck']//*[@class='sticker new']")));
-        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Red " +
-                "Duck']//*[@class='sticker sale']")));
-        //Blue Duck
-        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-most-popular']//*[@title='Blue Duck']//*[@class='sticker new']")));
-        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Blue " +
-                "Duck']//*[@class='sticker sale']")));
-        //Yellow Duck Campaigns
-        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-campaigns']//*[@title='Yellow Duck']//*[@class='sticker sale']")));
-        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Yellow " +
-                "Duck']//*[@class='sticker new']")));
-        //Yellow Duck Latest products
-        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Yellow Duck']//*[@class='sticker sale']")));
-        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Yellow " +
-                "Duck']//*[@class='sticker new']")));
-        //Green Duck Latest products
-        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Green " +
-                "Duck']//*[@class='sticker new']")));
-        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Green " +
-                "Duck']//*[@class='sticker sale']")));
-        //Red Duck Latest products
-        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Red " +
-                "Duck']//*[@class='sticker new']")));
-        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Red " +
-                "Duck']//*[@class='sticker sale']")));
-        //Blue Duck Latest products
-        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Blue " +
-                "Duck']//*[@class='sticker new']")));
-        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Blue " +
-                "Duck']//*[@class='sticker sale']")));
-        //Purple Duck Latest products
-        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Purple " +
-                "Duck']//*[@class='sticker new']")));
-        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Purple " +
-                "Duck']//*[@class='sticker sale']")));
+//        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-most-popular']//*[@title='Yellow Duck']//*[@class='sticker sale']")));
+//        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Yellow " +
+//                "Duck']//*[@class='sticker new']")));
+//        //Green Duck
+//        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-most-popular']//*[@title='Green Duck']//*[@class='sticker new']")));
+//        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Green " +
+//                "Duck']//*[@class='sticker sale']")));
+//        //Purple Duck
+//        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-most-popular']//*[@title='Purple Duck']//*[@class='sticker new']")));
+//        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Purple " +
+//                "Duck']//*[@class='sticker sale']")));
+//        //Red Duck
+//        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-most-popular']//*[@title='Red Duck']//*[@class='sticker new']")));
+//        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Red " +
+//                "Duck']//*[@class='sticker sale']")));
+//        //Blue Duck
+//        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-most-popular']//*[@title='Blue Duck']//*[@class='sticker new']")));
+//        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Blue " +
+//                "Duck']//*[@class='sticker sale']")));
+//        //Yellow Duck Campaigns
+//        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-campaigns']//*[@title='Yellow Duck']//*[@class='sticker sale']")));
+//        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Yellow " +
+//                "Duck']//*[@class='sticker new']")));
+//        //Yellow Duck Latest products
+//        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Yellow Duck']//*[@class='sticker sale']")));
+//        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Yellow " +
+//                "Duck']//*[@class='sticker new']")));
+//        //Green Duck Latest products
+//        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Green " +
+//                "Duck']//*[@class='sticker new']")));
+//        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Green " +
+//                "Duck']//*[@class='sticker sale']")));
+//        //Red Duck Latest products
+//        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Red " +
+//                "Duck']//*[@class='sticker new']")));
+//        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Red " +
+//                "Duck']//*[@class='sticker sale']")));
+//        //Blue Duck Latest products
+//        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Blue " +
+//                "Duck']//*[@class='sticker new']")));
+//        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Blue " +
+//                "Duck']//*[@class='sticker sale']")));
+//        //Purple Duck Latest products
+//        assertTrue(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Purple " +
+//                "Duck']//*[@class='sticker new']")));
+//        assertFalse(isOnlyOneElementPresent(By.xpath("//*[@id='box-latest-products']//*[@title='Purple " +
+//                "Duck']//*[@class='sticker sale']")));
     }
 
     @After
