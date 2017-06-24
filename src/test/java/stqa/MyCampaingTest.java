38495a71d67350b02a4f89820dc626bc459e7277
Nikita Tertytskyi <nikita.tertytskyi@gmail.com>
2017-06-24 17:19:13 +0200

    refactor campaing test

--- src/test/java/stqa/MyCampaingTest.java
+++ src/test/java/stqa/MyCampaingTest.java
@@ -5,14 +5,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by nikitatertytskyi on 23.06.17.
  */
-public class MyProductTest {
+public class MyCampaingTest {
     private WebDriver driver;
 
     @Before
@@ -23,9 +25,16 @@ public class MyProductTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://localhost/litecart/en/");
         driver.findElement(By.cssSelector("img[alt=\"My Store\"]"));
+        myCampaingTest();
     }
 
     @Test
+    public void myCampaingTest(){
+        List<WebElement> campaingRows = driver.findElements(By.cssSelector("div#box-campaigns li.product.column.shadow.hover-light"));
+        for(int i = 0; i<campaingRows.size(); i++){
+            WebElement element = driver.findElement(By.cssSelector(""));
+        }
+    }
 
 
     @After
